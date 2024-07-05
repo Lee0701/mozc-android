57e333e41da30e0fc2b933581f3033b6ff1035b2
Lee0701 <hgl1002@naver.com>
2024-07-05 17:19:52 +0900

    Migrate build files

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+import com.android.build.api.dsl.Packaging
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "org.mozc.android.inputmethod.japanese"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "org.mozc.android.inputmethod.japanese"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 6028152
+        versionName = "2.23.2815.103-arm"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    packaging {
+        resources.excludes.add("META-INF/DEPENDENCIES")
+    }
+}
+
+dependencies {
+    implementation(libs.guava)
+    implementation(libs.protobuf.java)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.httpclient)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
